fcd8f87a490096e9b4d7caf09805012c8fad689f
diogommartins <diogo.martins@unirio.br>
2015-05-18 18:08:09 -0300

    Initial Commit

---
+++ Fila de Prioridade/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Fila de Prioridade
+//
+//  Created by Diogo Martins on 5/18/15.
+//  Copyright (c) 2015 Diogo Martins. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
